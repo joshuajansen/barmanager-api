1dc9f1203ef377add974cd16ff1d9ef1ab704c21
Joshua Jansen <joshua@itflows.nl>
2012-10-08 23:24:48 +0200

    Initial commit with users and facebook login

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geobar::Application.config.secret_token = '6aa8f23e5a1525c10025bcb7efcd9de91ca2e5f00d69bf8cd1105f562274e401d90ee93320235118ce15e573929d676dd7be5d33e488b02effbabcf27370740f'
